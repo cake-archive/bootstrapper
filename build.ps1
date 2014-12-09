246f97e800616e0cebf13e7ef36fd887cdde7c21
Patrik Svensson <patrik@patriksvensson.se>
2014-12-10 00:45:43 +0100

    Updated build script.

--- build.ps1
+++ build.ps1
@@ -1,24 +1,3 @@
-<#
-Copyright (C) 2014 Patrik Svensson
-
-Permission is hereby granted, free of charge, to any person obtaining a copy of
-this software and associated documentation files (the "Software"), to deal in
-the Software without restriction, including without limitation the rights to
-use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
-the Software, and to permit persons to whom the Software is furnished to do so,
-subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in all
-copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
-FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
-COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
-IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
-CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-#>
-
 Param(
     [string]$Script = "build.cake",
     [string]$Target = "Default",
@@ -37,9 +16,13 @@ if (!(Test-Path $NUGET_EXE)) {
 
 # Restore tools from NuGet.
 Push-Location
-cd tools
-&nuget.exe install -ExcludeVersion
+Set-Location $TOOLS_DIR
+Invoke-Expression "$NUGET_EXE install -ExcludeVersion"
 Pop-Location
+if ($LASTEXITCODE -ne 0)
+{
+    exit $LASTEXITCODE
+}
 
 # Make sure that Cake has been installed.
 if (!(Test-Path $CAKE_EXE)) {
@@ -47,6 +30,5 @@ if (!(Test-Path $CAKE_EXE)) {
 }
 
 # Start Cake
-Write-Host
-&$CAKE_EXE $Script "-target=$Target" "-configuration=$Configuration" "-verbosity=$Verbosity"
-Write-Host
+Invoke-Expression "$CAKE_EXE `"$Script`" -target=`"$Target`" -configuration=`"$Configuration`" -verbosity=`"$Verbosity`""
+exit $LASTEXITCODE
