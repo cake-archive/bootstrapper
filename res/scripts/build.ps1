ed497fbd81514dd0a4734d3e8a8a7b1d6a9c45ec
Mattias Karlsson <mattias@devlead.se>
2015-06-29 17:08:18 +0200

    Fix WhatIf aliases was on Exerimental
    "DryRun" & "Noop" aliases moved to $WhatIf.

--- res/scripts/build.ps1
+++ res/scripts/build.ps1
@@ -31,8 +31,8 @@ Param(
     [string]$Configuration = "Release",
     [ValidateSet("Quiet", "Minimal", "Normal", "Verbose", "Diagnostic")]
     [string]$Verbosity = "Verbose",
-    [Alias("DryRun","Noop")]
     [switch]$Experimental,
+    [Alias("DryRun","Noop")]
     [switch]$WhatIf
 )
 
