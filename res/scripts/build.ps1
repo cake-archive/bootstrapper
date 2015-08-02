0381b192d4d46906dc2834f6bf5e8b35b52c398b
Patrik Svensson <patrik@patriksvensson.se>
2015-08-02 14:50:04 +0200

    Added better messages to bootstrapper script.

--- res/scripts/build.ps1
+++ res/scripts/build.ps1
@@ -36,17 +36,34 @@ Param(
     [switch]$Experimental,
     [Alias("DryRun","Noop")]
     [switch]$WhatIf,
-    [switch]$Mono
+    [switch]$Mono,
+    [switch]$Verbose
 )
+
+Write-Host "Preparing to run build script..."
+
+# Should we show verbose messages?
+if($Verbose.IsPresent)
+{
+    $VerbosePreference = "continue"
+}
 
 $TOOLS_DIR = Join-Path $PSScriptRoot "tools"
 $NUGET_EXE = Join-Path $TOOLS_DIR "nuget.exe"
 $CAKE_EXE = Join-Path $TOOLS_DIR "Cake/Cake.exe"
 $PACKAGES_CONFIG = Join-Path $TOOLS_DIR "packages.config"
 
+# Should we use mono?
+$UseMono = "";
+if($Mono.IsPresent) {
+    Write-Verbose -Message "Using the Mono based scripting engine."
+    $UseMono = "-mono"
+}
+
 # Should we use the new Roslyn?
 $UseExperimental = "";
-if($Experimental.IsPresent) {
+if($Experimental.IsPresent -and !($Mono.IsPresent)) {
+    Write-Verbose -Message "Using experimental version of Roslyn."
     $UseExperimental = "-experimental"
 }
 
@@ -56,12 +73,6 @@ if($WhatIf.IsPresent) {
     $UseDryRun = "-dryrun"
 }
 
-# Should we use mono?
-$UseMono = "";
-if($Mono.IsPresent) {
-    $UseMono = "-mono"
-}
-
 # Make sure tools folder exists
 if ((Test-Path $PSScriptRoot) -and !(Test-Path $TOOLS_DIR)) {
     New-Item -Path $TOOLS_DIR -Type directory
@@ -69,16 +80,17 @@ if ((Test-Path $PSScriptRoot) -and !(Test-Path $TOOLS_DIR)) {
 
 # Try find NuGet.exe in path if not exists
 if (!(Test-Path $NUGET_EXE)) {
-    "Trying to find nuget.exe in path"
-    $NUGET_EXE_IN_PATH = &where.exe nuget.exe
+    Write-Verbose -Message "Trying to find nuget.exe in path..."
+    ($NUGET_EXE_IN_PATH = &where.exe nuget.exe) | out-null
     if ($NUGET_EXE_IN_PATH -ne $null -and (Test-Path $NUGET_EXE_IN_PATH)) {
-        "Found $($NUGET_EXE_IN_PATH)"
+        "Found $($NUGET_EXE_IN_PATH)."
         $NUGET_EXE = $NUGET_EXE_IN_PATH 
     }
 }
 
 # Try download NuGet.exe if not exists
 if (!(Test-Path $NUGET_EXE)) {
+    Write-Verbose -Message "Downloading NuGet.exe..."
     Invoke-WebRequest -Uri http://nuget.org/nuget.exe -OutFile $NUGET_EXE
 }
 
@@ -94,15 +106,19 @@ $ENV:NUGET_EXE = $NUGET_EXE
 Push-Location
 Set-Location $TOOLS_DIR
 
+Write-Verbose -Message "Restoring tools from NuGet..."
+
 # Restore packages
 if (Test-Path $PACKAGES_CONFIG)
 {
-    Invoke-Expression "&`"$NUGET_EXE`" install -ExcludeVersion"
+    $NuGetOutput = Invoke-Expression "&`"$NUGET_EXE`" install -ExcludeVersion"
+    Write-Verbose $NuGetOutput
 }
 # Install just Cake if missing config
 else
 {
-    Invoke-Expression "&`"$NUGET_EXE`" install Cake -ExcludeVersion"
+    $NuGetOutput = Invoke-Expression "&`"$NUGET_EXE`" install Cake -ExcludeVersion"
+    Write-Verbose $NuGetOutput
 }
 Pop-Location
 if ($LASTEXITCODE -ne 0)
@@ -116,5 +132,6 @@ if (!(Test-Path $CAKE_EXE)) {
 }
 
 # Start Cake
+Write-Host "Running build script..."
 Invoke-Expression "$CAKE_EXE `"$Script`" -target=`"$Target`" -configuration=`"$Configuration`" -verbosity=`"$Verbosity`" $UseMono $UseDryRun $UseExperimental"
 exit $LASTEXITCODE
