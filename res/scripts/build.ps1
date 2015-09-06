7bfceac744ce23f8730364e38b95bb0f58e70ec5
Patrik Svensson <patrik@patriksvensson.se>
2015-09-06 19:48:30 +0200

    Added packages.config to bootstrapper.

--- res/scripts/build.ps1
+++ res/scripts/build.ps1
@@ -80,6 +80,12 @@ if ((Test-Path $PSScriptRoot) -and !(Test-Path $TOOLS_DIR)) {
     New-Item -Path $TOOLS_DIR -Type directory | out-null
 }
 
+# Make sure that packages.config exist.
+if (!(Test-Path $PACKAGES_CONFIG)) {
+    Write-Verbose -Message "Downloading packages.config..."
+    Invoke-WebRequest -Uri http://cakebuild.net/bootstrapper/packages -OutFile $PACKAGES_CONFIG
+}
+
 # Try find NuGet.exe in path if not exists
 if (!(Test-Path $NUGET_EXE)) {
     Write-Verbose -Message "Trying to find nuget.exe in path..."
